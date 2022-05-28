3d626e485088121b78d9e4efcb07c5c21343b026
Son Minh Nguyen <000433430@st>
2022-05-27 22:48:26 -0700

    add parallax effect to header and functionality for navbar tab

--- pages/components/Header/Navbar/Navbar.tsx
+++ pages/components/Header/Navbar/Navbar.tsx
@@ -1,17 +1,45 @@
 import styles from './Navbar.module.sass';
+import {useState} from 'react';
+
+interface Tab {
+    tabName: string;
+    tabActive: boolean;
+}
+
 export default function Navbar() {
+
+    //tabs state to keep track of the active tab
+    const [tabs, setTabs] = useState<Tab[]>([
+        { tabName: 'REVIEWS', tabActive: true },
+        { tabName: 'RESPONSE', tabActive: false },
+        { tabName: 'REUSE', tabActive: false },
+    ]);
+
+    //function to change the active tab
+    const changeTab = (tabName: string) => {
+        setTabs(
+            tabs.map(tab => {
+                if (tab.tabName === tabName) {
+                    tab.tabActive = true;
+                } else {
+                    tab.tabActive = false;
+                }
+                return tab;
+            })
+        );
+    }
+
     return (
         <nav className={styles.navbar}>
             <ul className={styles.navItems}>
-                <li className={`${styles.navItem} ${styles.navItemActive}`}>
-                    REVIEW
-                </li>
-                <li className={`${styles.navItem} ${styles.navItemInActive}`}>
-                    RESPONSE
-                </li>
-                <li className={`${styles.navItem} ${styles.navItemInActive}`}>
-                    REUSE
-                </li>
+                {tabs.map(tab => (
+                    <li 
+                    className={`${styles.navItem} ${tab.tabActive ? styles.navItemActive : ''}`}
+                    key={tab.tabName} 
+                    onClick={()=>changeTab(tab.tabName)}>
+                        {tab.tabName}
+                    </li>
+                ))}
             </ul>
         </nav>
     )
