71b8e13169bda15bd16c53d98a2ad03b8555edec
Phillip Chadwick <phillipchadwick@hotmail.com>
2022-05-31 19:41:21 -0700

    adds untested save method for all objects and adds dataAccess layer to file structure

---
+++ backEnd/dataAccessLayer/objectManager/objectManager.ts
@@ -0,0 +1,15 @@
+import { databaseObject } from "@/Models/Interfaces/databaseObject";
+import mongoose, { Model, Schema } from "mongoose";
+
+export class objectManager {
+
+    static saveObject(collectionName: string, obj: databaseObject,  schema: Schema) {
+
+        /// The hashMapped values of the object
+        const values = obj.toHashMap()
+        /// The base mongoose model to build the object with
+        const mongooseModel = mongoose.model(collectionName, schema)
+        /// the base mongoose model with values added to its fields
+        const finishedModel = new mongooseModel({values})
+    }
+}
