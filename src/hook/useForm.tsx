38e46cc6c1e2543dc2be45d50f31a2c56033607c
Son Minh Nguyen <000433430@student.vcc.ca>
2022-06-17 18:24:51 -0700

    add code for login and signup UI

--- src/hook/useForm.tsx
+++ src/hook/useForm.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState} from 'react';
 
 export default function useForm(initialState, validate, onSubmit) {
     const [form, setForm] = useState(initialState);
