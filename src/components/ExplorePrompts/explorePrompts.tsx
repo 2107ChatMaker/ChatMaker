67daff40380efbb874e4953429a5b4e6fe11fb24
abbeazale <abbeazale98@gmail.com>
2022-06-30 20:07:34 -0700

    creating explore prompts page

---
+++ src/components/ExplorePrompts/explorePrompts.tsx
@@ -0,0 +1,32 @@
+import SearchIcon from '@mui/icons-material/Search';
+import Image from 'next/image';
+import searchIcon from "./searchIcon.png"
+import styles from "./explorePrompts.module.sass"
+
+export default function ExplorePrompt(){
+
+    return(
+        <div className={styles.page}>
+            <div className={styles.pageTitle}>
+                Explore Prompts
+            </div>
+            <div className={styles.searchField}>
+                <input className={styles.input} placeholder="search" type="text" id="searchParam" size={50} name="sarchParam"/>
+                <button type="submit" className={styles.searchButton}>
+                    <Image className={styles.buttonImage} src={searchIcon} width={"20%"} height={"20%"} alt="seachImage"/>
+                </button>
+                <br/>
+            </div>
+            <div className={styles.prompts}>
+                fun prompt
+            </div>
+            <div className={styles.prompts}>
+                another 1
+            </div>
+            <div className={styles.prompts}>
+                :) 
+            </div>
+        </div>
+        
+    )
+}
