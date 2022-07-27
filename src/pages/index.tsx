210a80db5cc4c159b14ddbfcad990110c5242efb
KelseyVCC <83609351+KelseyVCC@users.noreply.github.com>
2022-07-27 03:15:52 -0700

    fixing load pt1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ import dynamic from 'next/dynamic';
 
 export default function Explore(props, {user, prompts}: HashMap) {
 
-  const LoadPrompts = dynamic(() => import('@components/Prompt/LoadPrompts'), {
+  const LoadPrompts = dynamic(() => import('@components/Prompt/LoadPromptsFIX'), {
     //will show a blue loading circle while loading in the content
     loading: () => <div><CircularProgress /></div>
   })
