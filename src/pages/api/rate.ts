6b32ea1e4c6a5807be86983ef3d2b692cc0cd064
Unfocused <unfocused@tuta.io>
2022-08-07 00:57:47 -0700

    rate console log to see crash error

--- src/pages/api/rate.ts
+++ src/pages/api/rate.ts
@@ -57,6 +57,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             let tags = [];
             let response = "";
             let prompt = "";
+            console.log('newResponseQuery', JSON.stringify(newResponseQuery));
 
             // if response exists
             if (!!newResponseQuery) {
